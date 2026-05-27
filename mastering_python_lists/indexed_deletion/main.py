7e295ee78480bbb35c5fdefc3a2fb43dc4d24042
SeyhanTech <>
2026-05-27 10:40:17 +0000

    Indexed deletion

---
+++ mastering_python_lists/indexed_deletion/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney", "Rome"]
+
+# Write your code below
+del travel_wishlist[travel_wishlist.index("Paris")]
+del travel_wishlist[travel_wishlist.index("Kyoto")]
+
+# Testing 
+print(travel_wishlist)
